82ed69eb24821140a95e96f583bb1f683ea66cf0
rahim4shanewaz <soumiq444@gmail.com>
2023-02-24 23:37:23 +0600

    modified

--- HomePage/HeroSection/HeroSection.js
+++ HomePage/HeroSection/HeroSection.js
@@ -1,9 +1,13 @@
+import { Typography } from '@mui/joy';
 import React from 'react';
 
 const HeroSection = () => {
     return (
         <div>
             <h2>This is Hero section where Hero Alom lives</h2>
+
+
+          
         </div>
     );
 };
