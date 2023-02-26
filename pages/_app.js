b5b4e013be460339bac6fef7f2aedcb7c31190e6
mdfaysal22 <mdfaysal.freelancer@gmail.com>
2023-02-26 14:37:07 +0600

    Footer and Team work Section Add

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import AuthProvider from "@/contexts/AuthProvider";
+import Footer from "@/Shared/Footer/Footer";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <>
       <Component {...pageProps} />
+      <Footer></Footer>
+      </>
     </AuthProvider>
   );
 }
